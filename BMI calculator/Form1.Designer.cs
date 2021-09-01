7fb551276f5a229f02de100ce0ff4718562539fe
1BrandonVanZyl1 <89069026+1BrandonVanZyl1@users.noreply.github.com>
2021-09-01 08:37:17 +0200

    Add project files.

---
+++ BMI calculator/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace BMI_calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.patientidTbox = new System.Windows.Forms.TextBox();
+            this.weightTbox = new System.Windows.Forms.TextBox();
+            this.heightTbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bmiTbox = new System.Windows.Forms.TextBox();
+            this.bmiLabel = new System.Windows.Forms.Label();
+            this.idElabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(174, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height (m)";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(303, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 31);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // patientidTbox
+            // 
+            this.patientidTbox.Location = new System.Drawing.Point(280, 69);
+            this.patientidTbox.Name = "patientidTbox";
+            this.patientidTbox.Size = new System.Drawing.Size(157, 22);
+            this.patientidTbox.TabIndex = 4;
+            // 
+            // weightTbox
+            // 
+            this.weightTbox.Location = new System.Drawing.Point(280, 136);
+            this.weightTbox.Name = "weightTbox";
+            this.weightTbox.Size = new System.Drawing.Size(157, 22);
+            this.weightTbox.TabIndex = 5;
+            // 
+            // heightTbox
+            // 
+            this.heightTbox.Location = new System.Drawing.Point(280, 193);
+            this.heightTbox.Name = "heightTbox";
+            this.heightTbox.Size = new System.Drawing.Size(157, 22);
+            this.heightTbox.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(177, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Weight (Kg)";
+            // 
+            // bmiTbox
+            // 
+            this.bmiTbox.Location = new System.Drawing.Point(560, 135);
+            this.bmiTbox.Name = "bmiTbox";
+            this.bmiTbox.Size = new System.Drawing.Size(77, 22);
+            this.bmiTbox.TabIndex = 8;
+            // 
+            // bmiLabel
+            // 
+            this.bmiLabel.AutoSize = true;
+            this.bmiLabel.ForeColor = System.Drawing.Color.Black;
+            this.bmiLabel.Location = new System.Drawing.Point(669, 138);
+            this.bmiLabel.Name = "bmiLabel";
+            this.bmiLabel.Size = new System.Drawing.Size(0, 16);
+            this.bmiLabel.TabIndex = 9;
+            // 
+            // idElabel
+            // 
+            this.idElabel.AutoSize = true;
+            this.idElabel.ForeColor = System.Drawing.Color.Red;
+            this.idElabel.Location = new System.Drawing.Point(277, 94);
+            this.idElabel.Name = "idElabel";
+            this.idElabel.Size = new System.Drawing.Size(0, 16);
+            this.idElabel.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(580, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 16);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "BMI";
+            this.label4.Click += new System.EventHandler(this.label4_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.idElabel);
+            this.Controls.Add(this.bmiLabel);
+            this.Controls.Add(this.bmiTbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.heightTbox);
+            this.Controls.Add(this.weightTbox);
+            this.Controls.Add(this.patientidTbox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox patientidTbox;
+        private System.Windows.Forms.TextBox weightTbox;
+        private System.Windows.Forms.TextBox heightTbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox bmiTbox;
+        private System.Windows.Forms.Label bmiLabel;
+        private System.Windows.Forms.Label idElabel;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ BMI calculator/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace BMI_calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public float calculate(float weight, float height)
+        {
+
+            float bmi = weight / (height * height);
+
+            return bmi;
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                float bmi = calculate(float.Parse(weightTbox.Text), float.Parse(heightTbox.Text));
+                bmiTbox.Text = bmi.ToString();
+
+
+                if (bmi < 18.5)
+                {
+                    bmiLabel.Text = "Under Weight";
+
+                }
+                else if (bmi >= 18.5 && bmi <= 24.9)
+                {
+                    bmiLabel.Text = "Healthy";
+                }
+                else if (bmi >= 25 && bmi <= 29.9)
+                {
+                    bmiLabel.Text = "Over Weight";
+                }
+
+                else
+                {
+                    bmiLabel.Text = "Obese";
+
+                }
+
+
+                if (patientidTbox.Text.Length < 13 || patientidTbox.Text.Length > 13)
+                {
+                    idElabel.Text = "Enter a valid ID number";
+                }
+
+                else
+                {
+                    try
+                    {
+                        idElabel.Text = "";
+
+                        String str = "Data Source=LAPTOP-Q1L5OKJ1;Initial Catalog=BMIcalculator;Integrated Security=True";
+
+                        String query = "Insert into newPatients (patientId, weight, height,[bmi/comment]) VALUES ( @patientId, @weight, @height,@bmi)"; ;
+
+                        SqlConnection con = new SqlConnection(str);
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+
+                        con.Open();
+                        cmd.Parameters.AddWithValue("@patientId", patientidTbox.Text);
+                        cmd.Parameters.AddWithValue("@weight", weightTbox.Text);
+                        cmd.Parameters.AddWithValue("height", heightTbox.Text);
+                        cmd.Parameters.AddWithValue("@bmi", bmiLabel.Text);
+
+
+
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Record is saved", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+                        DataSet ds = new DataSet();
+
+
+
+
+
+                        con.Close();
+                    }
+                    catch (Exception es)
+                    {
+
+                        MessageBox.Show(es.Message);
+                    }
+                }
+
+            }
+            catch (Exception es)
+            {
+
+                MessageBox.Show(es.Message);
+
+
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
